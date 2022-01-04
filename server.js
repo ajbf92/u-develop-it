40d6e8941929a173ebc659592074d2db85b74634
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-01-04 17:42:28 -0500

    format

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
-const express = require("express");
+const mysql = require('mysql2');
+const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "",
+    database: "election"
+  },
+  console.log("Connected to the election database.")
+);
+
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+  if (err) {
+        console.log(err);
+      }
+console.log(rows);
+});
+
+// // GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
