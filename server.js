5ef4f167e486999d6df9ec3652571750af91bc7d
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-01-05 18:28:53 -0500

    query for creating, selecting a single candidate, and deleting a candidate

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ db.query('SELECT * FROM candidates', (err, rows) => {
 console.log(rows);
 });
 
-// // GET a single candidate
+// GET a single candidate
 // db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
 //   if (err) {
 //     console.log(err);
@@ -37,10 +37,30 @@ console.log(rows);
 //   console.log(row);
 // });
 
-// Default response for any other request (Not Found)
-// app.use((req, res) => {
-//   res.status(404).end();
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
 // });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
