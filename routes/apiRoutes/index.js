c9925813db5a3d3b1915d16e76e93ed26e6db649
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-01-16 12:02:47 -0500

    created a router for api endpoints and separated candidate and party routes away from server.js file

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.use(require('./candidateRoutes'));
+router.use(require("./candidateRoutes"));
+router.use(require("./partyRoutes"));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,69 +1,17 @@
-const db = require('./db/connection');
+const db = require("./db/connection");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const inputCheck = require("./utils/inputCheck");
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require("./routes/apiRoutes");
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Add after Express middleware
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
-// gets all parties
-app.get("/api/parties", (req, res) => {
-  const sql = `SELECT * FROM parties`;
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: rows,
-    });
-  });
-});
-
-// gets party by an id
-app.get("/api/party/:id", (req, res) => {
-  const sql = `SELECT * FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.query(sql, params, (err, row) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: row,
-    });
-  });
-});
-
-// deletes a party
-app.delete("/api/party/:id", (req, res) => {
-  const sql = `DELETE FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: res.message });
-      // checks if anything was deleted
-    } else if (!result.affectedRows) {
-      res.json({
-        message: "Party not found",
-      });
-    } else {
-      res.json({
-        message: "deleted",
-        changes: result.affectedRows,
-        id: req.params.id,
-      });
-    }
-  });
-});
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
